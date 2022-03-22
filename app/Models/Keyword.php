9ba16bd3fa2492f2c424178852e3df5137905b55
hagaitrg <disahagait@gmail.com>
2022-03-22 14:06:42 +0700

    setup model and migration for keywords and products

--- app/Models/Keyword.php
+++ app/Models/Keyword.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Keyword extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id','name'];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
