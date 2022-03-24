71573c9e31f8ec6d2486212c615b9ebec1db94e3
hagaitrg <disahagait@gmail.com>
2022-03-24 22:08:13 +0700

    cleansing admin dashboard

---
+++ resources/views/admin/layouts/nav.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg main-navbar">
+    <ul class="navbar-nav mr-3">
+        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+    </ul>
+    <ul class="navbar-nav navbar-right ml-auto">
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="{{ asset('cms/img/avatar/avatar-2.png') }}" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">Hi, Super Admin</div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="#" class="dropdown-item has-icon text-danger">
+                    <i class="fas fa-sign-out-alt"></i> Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('cms')->group(function(){
+    Route::prefix('admin')->group(function(){
+        Route::get('index', function(){
+            return view('admin.index');
+        })->name('admin-index');
+    });
 });
