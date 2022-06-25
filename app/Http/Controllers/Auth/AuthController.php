82c6dfa9fd641d66abfad5ad651b1cb79c45e866
hagaitrg <disahagait@gmail.com>
2022-06-26 02:11:50 +0700

    fixing migrations and moddel resotran

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\sendEmail;
 
 class AuthController extends Controller
 {
