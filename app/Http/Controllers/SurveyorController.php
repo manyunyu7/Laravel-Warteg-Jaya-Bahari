0bd26797e52850343aae8cb0b22873d0be522e92
Henry Augusta <henryaugusta4@gmail.com>
2023-07-20 14:28:35 +0700

    Add testing route

--- app/Http/Controllers/SurveyorController.php
+++ app/Http/Controllers/SurveyorController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Restoran;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class SurveyorController extends Controller
 {
     public function testing()
     {
+        Artisan::call('cache:clear');
+        Artisan::call('optimize');
+        Artisan::call('route:cache');
+        Artisan::call('route:clear');
+        Artisan::call('view:clear');
+        Artisan::call('config:cache');
+        Artisan::call('config:clear');
+        return "All Cache Cleared !!!";
+
         $restorans = Restoran::all()->filter(function ($restoran) {
             return !is_null($restoran->data_bangunan);
         })->values();
 
         return $restorans;
+
+
+
     }
 }
