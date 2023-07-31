8baa6cb8a6457fced8b299e09540dca7eb34e320
Henry Augusta <henryaugusta4@gmail.com>
2023-07-31 19:51:53 +0700

    add flag on restos

---
+++ database/migrations/2023_07_31_194630_add_flag_column_to_restos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFlagColumnToRestos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('restos', function (Blueprint $table) {
+            $table->string('flag')->nullable()->after('address'); // Adjust 'column_name' to the column where you want the new column to be placed
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('restos', function (Blueprint $table) {
+            $table->dropColumn('flag');
+        });
+    }
+}
